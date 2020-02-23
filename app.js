045b514cc4de840787f5938172289dc1fdfee4c6
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-02-23 15:52:35 +0530

    updated js file - added accessing the API

--- app.js
+++ app.js
@@ -1,30 +1,56 @@
-window.addEventListener('load', ()=> {
+window.addEventListener("load", ()=> {
   let long;
   let lat;
-  let temperatureDescription = document.querySelector('.temperature-description');
-  let temperatureDegree = document.querySelector('.temperature-degree');
-  let locationTimezone = document.querySelector('.location-timezone');
+  let temperatureDescription = document.querySelector(".temperature-description");
+  let temperatureDegree = document.querySelector(".temperature-degree");
+  let locationTimezone = document.querySelector(".location-timezone");
+  let temperatureSection = document.querySelector(".temperature");
+  let temperatureSpan = document.querySelector(".temperature span");
 
   if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(position =>{
       long = position.coords.longitude;
       lat = position.coords.latitude;
-      const proxy = 'http://cors-anywhere.herokuapp.com/'
+      const proxy = "http://cors-anywhere.herokuapp.com/"
       const api = `${proxy}https://api.darksky.net/forecast/3a14ea2f87bf92336f09df6099ee2565/${lat},${long}`;
 
       fetch(api)
-        .then(response =>{
-          return response.json;
+        .then(response => {
+          return response.json();
         })
-        .then(data =>{
-          
-          const{ temperature, summary } = data.currently;
-          temperatureDegree.textcontent = temperature;
+        .then(data => {
+          console.log(data);
+          const{ temperature, summary, icon } = data.currently;
+          //Set DOM ELements from the API
+          temperatureDegree.textContent = temperature;
+          temperatureDescription.textContent = summary;
+          locationTimezone.textContent = data.timezone;
+          //FORMUlA for celcuis
+          let celcius = (temperature - 32) * (5/9);
+          //Set Icon
+          setIcons(icon, document.querySelector(".icon"));
+
+          //Change temp to celcuis/fahrenhiet
+          temperatureSection.addEventListener("click", () => {
+            if(temperatureSpan.textContent ==="F"){
+              temperatureSpan.textContent = "C";
+              temperatureDegree.textContent = Math.floor(celcius);
+            } else {
+              temperatureSpan.textContent = "F";
+              temperatureDegree.textContent = temperature;
+            }
+
+          })
         });
     });
-
-
-  }else{
+  }  else{
     h1.textcontent = "Hey, either your browser does not supports geolocation OR you denied permission to it."
+  }
+
+  function setIcons(icon, iconID) {
+    const skycons = new Skycons({color: "white"});
+    const currentIcon = icon.replace(/-/g,"_").toUpperCase();
+    skycons.play();
+    return skycons.set(iconID, Skycons[currentIcon]);
   }
 });
